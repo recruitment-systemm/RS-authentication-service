77285e39aedc7c57e594d35e60745c1337964d3f
ahmedali109 <ahmedalinaguib33@gmail.com>
2026-08-13 15:38:30 +0300

    Configure Cloudinary

---
+++ src/main/java/org/example/authenticationservice/config/CloudinaryConfig.java
@@ -0,0 +1,21 @@
+package org.example.authenticationservice.config;
+
+import com.cloudinary.Cloudinary;
+import org.example.authenticationservice.config.Properties.CloudinaryProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class CloudinaryConfig {
+    @Bean
+    public Cloudinary cloudinary(CloudinaryProperties properties) {
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", properties.getCloudName());
+        config.put("api_key", properties.getApiKey());
+        config.put("api_secret", properties.getApiSecret());
+        return new Cloudinary(config);
+    }
+}
